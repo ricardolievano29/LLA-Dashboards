f1fc8716901f96836ef2db96f529d4c24d0a2d4d
ricardolievano29 <118026222+ricardolievano29@users.noreply.github.com>
2022-11-18 14:00:37 -0500

    Create Punto 5.sql

---
+++ Modulo 4 Capacitación/Punto 5.sql
@@ -0,0 +1,10 @@
+Select
+account_id,
+date_diff('DAY',  cast (concat(substr(oldest_unpaid_bill_dt, 1,4),'-',substr(oldest_unpaid_bill_dt, 5,2),'-', substr(oldest_unpaid_bill_dt, 7,2)) as date), cast(dt as date)),
+case when date_diff('DAY',  cast (concat(substr(oldest_unpaid_bill_dt, 1,4),'-',substr(oldest_unpaid_bill_dt, 5,2),'-', substr(oldest_unpaid_bill_dt, 7,2)) as date), cast(dt as date)) > 90 OR date_diff('DAY',  cast (concat(substr(oldest_unpaid_bill_dt, 1,4),'-',substr(oldest_unpaid_bill_dt, 5,2),'-', substr(oldest_unpaid_bill_dt, 7,2)) as date), cast(dt as date)) is null then 'Inactivo'
+when date_diff('DAY',  cast (concat(substr(oldest_unpaid_bill_dt, 1,4),'-',substr(oldest_unpaid_bill_dt, 5,2),'-', substr(oldest_unpaid_bill_dt, 7,2)) as date), cast(dt as date))<=90 then 'Activo' end as Activo_Inactivo
+from "db-analytics-prod"."tbl_postpaid_cwc"
+where  dt  = '2022-01-10'
+
+and org_id = '338'
+and account_type = 'Residential'
