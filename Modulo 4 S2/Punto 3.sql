e80ffbf35e05d5d546b3d1b18efba2d4a3f2c044
ricardolievano29 <118026222+ricardolievano29@users.noreply.github.com>
2022-11-23 08:42:59 -0500

    Update Punto 3.sql

--- Modulo 4 S2/Punto 3.sql
+++ Modulo 4 S2/Punto 3.sql
@@ -7,8 +7,8 @@ and date(dt)=date_trunc('MONTH',date(dt))
 )
 
 select distinct date_trunc('MONTH', date(u.dt)) as mes,count(distinct u.users) as service_orders_interactions
-from usuarios u left join "db-stage-prod"."interactions_cwp" i on users = i.account_id left join "db-stage-dev"."so_hdr_cwp" s on users =  cast(s.account_id as VARCHAR) and date_trunc('MONTH',date(u.dt)) = date_trunc('MONTH',date(i.dt))
-where date(u.dt)= date_trunc('MONTH', date(u.dt))
+from usuarios u left join "db-stage-prod"."interactions_cwp" i on users = i.account_id and date_trunc('MONTH',date(u.dt)) = date_trunc('MONTH',date(i.dt))
+    left join "db-stage-dev"."so_hdr_cwp" s on users =  cast(s.account_id as VARCHAR) and date_trunc('MONTH',date(u.dt)) = date_trunc('MONTH',date(s.dt))
 and date(u.dt)>= date('2022-01-01')
 and i.account_id is not null and s.account_id is not null
 group by 1 order by 1
